b676b52dc62727c0e1bfede21ae887b45fd072e8
viktorowich <schneider.viktor.88@googlemail.com>
2016-05-23 13:51:09 +0200

    added ChatAdapter

--- app/src/main/java/berlin/htw/schneider/viktor/sharknet/ChatListAdapter.java
+++ app/src/main/java/berlin/htw/schneider/viktor/sharknet/ChatListAdapter.java
@@ -1,7 +1,37 @@
 package berlin.htw.schneider.viktor.sharknet;
 
-/**
- * Created by viktorowich on 23/05/16.
- */
-public class ChatListAdapter {
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+
+import java.util.LinkedList;
+
+public class ChatListAdapter extends ArrayAdapter<Chat>
+{
+    private LinkedList<> chats;
+
+    public ChatListAdapter(Context context, int resource, LinkedList<Chat> objects) {
+        super(context, resource, objects);
+        chats = objects;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent)
+    {
+        if(convertView == null)
+        {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.line_item_chat,parent,false);
+        }
+
+        Chat chat = chats.get(position);
+        TextView name = (TextView) convertView.findViewById(R.id.name);
+        //name.setText(chat.getMes);
+
+        return super.getView(position, convertView, parent);
+    }
 }
