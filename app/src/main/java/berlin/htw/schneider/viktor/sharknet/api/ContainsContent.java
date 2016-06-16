2458bf3accce0eaa12354701b15e9fe338692202
viktorowich <schneider.viktor.88@googlemail.com>
2016-06-16 11:50:55 +0200

    build.gradle include the subproject

--- app/src/main/java/berlin/htw/schneider/viktor/sharknet/api/ContainsContent.java
+++
@@ -1,9 +0,0 @@
-package berlin.htw.schneider.viktor.sharknet.api;
-
-/**
- * Created by timol on 06.06.2016.
- */
-public interface  ContainsContent {
-
-	public Content getContent();
-}
--- app/src/main/java/berlin/htw/schneider/viktor/sharknet/api/Setting.java
+++
@@ -1,7 +0,0 @@
-package berlin.htw.schneider.viktor.sharknet.api;
-
-/**
- * Created by timol on 12.05.2016.
- */
-public interface Setting {
-}
