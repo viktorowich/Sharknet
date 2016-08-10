732c19713aff6a885c269d6427915001e28024e7
viktorowich <schneider.viktor.88@googlemail.com>
2016-08-10 02:11:43 +0200

    chatdetail an das design angepasst

--- app/src/main/java/berlin/htw/schneider/viktor/sharknet/MsgListAdapter.java
+++ app/src/main/java/berlin/htw/schneider/viktor/sharknet/MsgListAdapter.java
@@ -1,19 +1,14 @@
 package berlin.htw.schneider.viktor.sharknet;
 
-import android.content.Context;
 import android.graphics.Color;
-import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
 import android.support.v7.widget.RecyclerView;
 import android.text.SpannableStringBuilder;
 import android.text.style.ImageSpan;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.TextView;
 import net.sharksystem.sharknet.api.Message;
 
@@ -22,46 +17,104 @@ import java.util.List;
 /**
  * Created by viktorowich on 01/06/16.
  */
-public class MsgListAdapter extends RecyclerView.Adapter<MsgListAdapter.MyViewHolder>
+public class MsgListAdapter extends RecyclerView.Adapter<MsgListAdapter.ViewHolderBase>
 {
     private List<Message> msgs;
-    Drawable check,cross;
+    //Drawable check,cross;
 
     public MsgListAdapter( List<Message> objects)
     {
         msgs = objects;
     }
 
-    public class MyViewHolder extends RecyclerView.ViewHolder
+    public class ViewHolderBase extends RecyclerView.ViewHolder
     {
         public TextView msg;
 
-
-        public MyViewHolder(View itemView)
+        public ViewHolderBase(View itemView)
         {
             super(itemView);
             msg = (TextView) itemView.findViewById(R.id.msg);
-             check = itemView.getResources().getDrawable(R.drawable.ic_check_green_600_18dp);
-             cross = itemView.getResources().getDrawable(R.drawable.ic_close_red_300_18dp);
+            //check = itemView.getResources().getDrawable(R.drawable.ic_check_green_600_18dp);
+           // cross = itemView.getResources().getDrawable(R.drawable.ic_close_red_300_18dp);
+        }
+    }
+
+    public class ViewHolderMsg extends ViewHolderBase
+    {
+        public TextView msg;
+
+        public ViewHolderMsg(View itemView)
+        {
+            super(itemView);
+            msg = (TextView) itemView.findViewById(R.id.msg);
+            //check = itemView.getResources().getDrawable(R.drawable.ic_check_green_600_18dp);
+            //cross = itemView.getResources().getDrawable(R.drawable.ic_close_red_300_18dp);
+        }
+    }
+
+    public class ViewHolderMsgMy extends ViewHolderBase
+    {
+        public TextView msg;
+
+        public ViewHolderMsgMy(View itemView)
+        {
+            super(itemView);
+            msg = (TextView) itemView.findViewById(R.id.msg);
+            //check = itemView.getResources().getDrawable(R.drawable.ic_check_green_600_18dp);
+            //cross = itemView.getResources().getDrawable(R.drawable.ic_close_red_300_18dp);
+        }
+    }
+
+    /**
+     * Check if i am the creator of the message.
+     *
+     * @param position  Position of the Message from the List of all Messages
+     *
+     * @return int viewType: 0 not my Message
+     *                       1 my Message
+     */
+    @Override
+    public int getItemViewType(int position)
+    {
+        Message message = msgs.get(position);
+        if(message.isMine())
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
         }
     }
 
     @Override
-    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
+    public ViewHolderBase onCreateViewHolder(ViewGroup parent, int viewType)
     {
-        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.line_item_msg,parent,false);
-        return new MyViewHolder(itemView);
+        View itemView;
+        switch (viewType)
+        {
+            case 0:
+                itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.line_item_msg,parent,false);
+                return new ViewHolderMsg(itemView);
+            case 1:
+                itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.line_item_msg_my,parent,false);
+                return new ViewHolderMsgMy(itemView);
+
+        }
+        itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.line_item_msg,parent,false);
+        return new ViewHolderMsg(itemView);
     }
 
     @Override
-    public void onBindViewHolder(MyViewHolder holder, int position)
+    public void onBindViewHolder(ViewHolderBase holder, int position)
     {
         Message message = msgs.get(position);
         SpannableStringBuilder builder = new SpannableStringBuilder();
-        String s = new java.text.SimpleDateFormat("HH:mm").format(message.getTimestamp());
-        builder.append(message.getContent().getMessage()).append("         ");
-        builder.append(s).append("   ");
-
+        //String s = new java.text.SimpleDateFormat("HH:mm").format(message.getTimestamp());
+        builder.append(message.getContent().getMessage());
+        //builder.append(s).append("   ");
+/*
         assert check != null;
         check.setBounds(0, 0, holder.msg.getLineHeight(),holder.msg.getLineHeight());
         assert cross != null;
@@ -111,6 +164,7 @@ public class MsgListAdapter extends RecyclerView.Adapter<MsgListAdapter.MyViewHo
             builder.setSpan(new ImageSpan(cross),builder.length() - 3,builder.length(),0);
 
         }
+        */
         holder.msg.setText(builder);
     }
 
